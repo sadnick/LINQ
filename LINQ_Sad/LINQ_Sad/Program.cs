e200b32a80fb1c661ef61727d8c36e63b9a3b539
Sadyrinnik <82495558+sadnick@users.noreply.github.com>
2022-02-10 14:04:38 +0300

    Add files via upload

---
+++ LINQ_Sad/LINQ_Sad/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Sad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            string[] teams = { "Бавария", "Белоруссия", "Мадрид", "Манчестер Сити", "ПСЖ", "Барселона" };
+
+            var selectedTeams = from a in teams // определяем каждый объект из teams как a
+                                where a.ToUpper().StartsWith("М") //фильтрация по критерию
+                                orderby a  // упорядочиваем по возрастанию
+                                select a; // выбираем объект
+
+            foreach (string s in selectedTeams)
+                Console.WriteLine(s);
+            Console.ReadKey();
+            
+        }
+
+    }
+}
