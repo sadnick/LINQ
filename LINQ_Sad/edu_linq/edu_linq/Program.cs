32c8fce028cb3fad5b7ea887048d79914b83be8b
Sadyrinnik <82495558+sadnick@users.noreply.github.com>
2022-02-11 14:24:26 +0300

    Add files via upload

---
+++ LINQ_Sad/edu_linq/edu_linq/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace edu_linq
+{
+    class User
+    {
+            public string Name { get; set; }
+            public int Age { get; set; }
+        
+        static void Main(string[] args)
+        {
+            List<User> users = new List<User>();
+            users.Add(new User { Name = "Sam", Age = 43 });
+            users.Add(new User { Name = "Tom", Age = 33 });
+
+            var names = from u in users select u.Name;
+
+            foreach (string n in names)
+                Console.WriteLine(n);
+            List<User> user = new List<User>();
+            users.Add(new User { Name = "Sam", Age = 43 });
+            users.Add(new User { Name = "Tom", Age = 33 });
+
+            var items = from u in users
+                        select new
+                        {
+                            FirstName = u.Name,
+                            DateOfBirth = DateTime.Now.Year - u.Age
+                        };
+
+            foreach (var n in items)
+                Console.WriteLine($"{n.FirstName} - {n.DateOfBirth}");
+            Console.ReadKey();
+
+        }
+
+        
+    
+    }
+    
+            
+       
+        
+}
